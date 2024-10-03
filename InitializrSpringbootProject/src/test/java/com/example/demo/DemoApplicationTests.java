45c9ae6afd1c0f36b862bcaa6c1da60a4e775de8
LuisCalvo283 <lcalvo30664@ufide.ac.cr>
2024-10-02 20:51:29 -0600

    Semana-3.1
    Cambios de semana 3
    -quedan pendientes

--- InitializrSpringbootProject/src/test/java/com/example/demo/DemoApplicationTests.java
+++ InitializrSpringbootProject/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,10 +1,10 @@
-package Tienda.demo;
+package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TiendaLuisCalvoHerraApplicationTests {
+class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
---
+++ Tienda-Luis_Calvo_Herra/src/test/java/Tienda_Luis_Calvo_Herra/demo/TiendaApplicationTests.java
@@ -0,0 +1,13 @@
+package Tienda_Luis_Calvo_Herra.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
